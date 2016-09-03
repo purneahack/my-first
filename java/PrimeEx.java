9fdb92acb396e3872e8b7a4404c5ec311053b938
mimran <imran.mohammad@booking.com>
2016-09-03 21:13:35 +0200

    added comments for prime number program

--- java/PrimeEx.java
+++ java/PrimeEx.java
@@ -2,19 +2,21 @@ class PrimeEx
 {
 	public static void main(String[] args)
 	{
-		int i,m=0,flag=0;
-		int n=15;//it isthe number to be checked also how to input value of any random number
-		m=n/2;
-		for(i=2;i<=m;i++)
-		{
-			if(n%i==0)
-			{
-				System.out.println("Number is not prime");
-				flag=1;
+		int i,m=0,myFlagWithAnyName=0;
+		int n=15;//it is the number to be checked also how to input value of any random number
+		m=n/2; //integer division 
+		i=2;
+		for(;i<=m;i++){
+			if(n%i==0){
+				String text = "Number is not prime";
+				System.out.println("Number is not prime"); //calling a function named  "println"
+				//System.out.println(text); //above line is same
+				myFlagWithAnyName=1; 
 				break;
 			}
 		}
-		if(flag==0)
+		if(myFlagWithAnyName==0){
 			System.out.println("Number is prime");
+		}
 	}
 }
--- java/prime.java
+++ java/prime.java
@@ -1,17 +1,21 @@
-class prime
+class Prime // capital name for any class conventionally 
 {
-	public static void main ( String[] args)
-	int x = 89
-	int n = [2,3,5,7]
-	for (i = 0; i <= 3; i++)
-	{
-	if (x%n[i]==0)
-	{
-		System.out.println(" this is notprime no " +n)
-}
-	} 
-	else if (x%x==1)
-	{
-		System.out.println(" this is prime no " +n)
+	//public is modifier ... basically visibility 
+	//static means it is class property and can be invoked with class name // search for concept of object and classes in JAVA
+	//void here is return type of the funcion named as "main"
+	//main is the special "function" in java from where the execution starts
+	//String is a class is java
+	//args (kuchhBhi) is the variable name here and it is for an array of strings
+	public static void main ( String[] kuchhBhi) {
+		//function body
+		int x = 89;
+		int n = [2,3,5,7];
+		for (i = 0; i <= 3; i++){
+			if (x%n[i]==0){
+				System.out.println(" this is not prime no " +n);
+			}else if (x%x==1){
+				System.out.println(" this is prime no " +n);
+			}
+		}
 	}
 }
