36f74591f8da9472a19edebf5ac187fd24ad35e1
purneahack <shahnawazetc@gmail.com>
2018-01-15 15:53:11 +0530

    Add files via upload

--- ajax.php
+++ ajax.php
@@ -1,55 +1,22 @@
 <?php
-
-//checks to see if a particular variable exists:		
-if(isset($_POST['state'])){
-    //it exists
-    echo $_POST['state'];
-} else{
-    echo "it does not exist.";
-}
-
-// array_key_exists, which checks to see if a particular index exists:
-if(array_key_exists('state', $_POST)){
-    //it exists
-    echo $_POST['state'];
-} else{
-    echo "array key does not exist";
-}
-
-//$state = (isset($_POST['state']) ? $_POST['state'] : null);
-
-//if(isset($_POST["state"])){
-	//$state=$_POST["state"];
-//}
-
-
-//$state=$_POST['state']; //my query is not working. I am not able to get $sate value. here $sate value cannot get the value.
-$state = intval($_POST['state']);
-//$state=$_REQUEST["state"];
-
-
-$servername = "localhost";
-$username = "root";
-$password = "moinuddin@1";
-$dbname = "slcsa";
 $link = mysqli_connect('localhost', 'root', 'moinuddin@1', 'slcsa');
+
 if (!$link) {
 			die("Connection failed: " . mysqli_connect_error());
 		}
 		echo "Connection to database established";
 
+$state=$_POST['state']; //my query is not working. I am not able to get $sate value. here $sate value cannot get the value.
 
-	//if ($state!=""){	
+	if ($state!=""){	
 	//if (!empty($state)){
 		//$res = mysqli_query($link,"SELECT * FROM district_code WHERE State Code='$state'");
 		$res = mysqli_query($link,"SELECT * FROM district_code WHERE State Code = '".$state."'");
-		echo "<select>";
-			//while($row = mysqli_fetch_array($res)){
+		echo "<select>";			
 			while($row = mysqli_fetch_array($res)){
 				echo "<option>"; echo $row["District Name"]; echo "</option>";
 			}
 		echo "</select>";
-	//}
+	}	
 	
-	//mysqli_close($link);
 ?>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $link = mysqli_connect('localhost', 'root', 'moinuddin@1', 'slcsa');
 	<table>
 		<tr>
 			<td>Select State</td>
-			<td><select id="statedd" class="statedd" onchange="change_state(this.value);"> <!--we will add onchange to perform Ajax using function change_state()-->
+			<td><select id="statedd" class="statedd" onchange="change_state(this.value)">
 				<option value="">Select</option>
 				<?php 
 					$res = mysqli_query($link,"SELECT * FROM state_code");
@@ -36,62 +36,13 @@ $link = mysqli_connect('localhost', 'root', 'moinuddin@1', 'slcsa');
 <script src="jquery.min.js"></script>
 
 <script type="text/javascript">
-//function change_state(st)
-//function change_state()
-//{
-	//var xmlhttp=new XMLHttpRequest();
-	//xmlhttp.open("POST", "ajax.php?state="+st, false);
-	//xmlhttp.open("POST", "ajax.php?state="+document.getElementById("statedd").value, false);
-	//xmlhttp.send(null);	
-	//document.getElementById("districtdd").innerHTML=xmlhttp.responseText;
-	
-//}
 
-function change_state(val)
+function change_state()
 {
-	var state = arguments[0];
-	$('#districtdd').empty();
-	$.ajax({
-		method:'POST',
-		url:'ajax.php',
-		data:'State Code='+val,
-		success:function(data)
-		{
-			alert("Inside success");
-			for(i=0;i<data.length ; i++)
-			{
-				alert(data.text);
-				$('#districtdd').append("<option value = '"+data[i].value+"'>"+data[i].text+"</option>")
-			}
-		},
-		error:function()
-		{
-			alert("Something went wrong");
-		}
-	}
-	);
-	
+	var xmlhttp=new XMLHttpRequest();	
+	xmlhttp.open("POST", "ajax.php?state="+document.getElementById("statedd").value, false);
+	xmlhttp.send(null);	
+	document.getElementById("districtdd").innerHTML=xmlhttp.responseText;	
 }
-
-   // if (str == "") {
-       // document.getElementById("districtdd").innerHTML = "";
-       // return;
-   // } else { 
-       // if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-         //   xmlhttp = new XMLHttpRequest();
-       // } else {
-            // code for IE6, IE5
-          //  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-       // }
-        //xmlhttp.onreadystatechange = function() {
-           // if (this.readyState == 4 && this.status == 200) {
-               // document.getElementById("districtdd").innerHTML = this.responseText;
-           // }
-       // };
-        //xmlhttp.open("POST","ajax.php?state="+str,true);
-       // xmlhttp.send();
-    //}
-//}
 
 </script>
